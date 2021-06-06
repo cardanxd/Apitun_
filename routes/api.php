bda5607876d42e654f418876cbf99f40095325fa
Carlos Lopez <carloslopezp1497@gmail.com>
2021-06-06 11:42:27 -0500

    Backup api

--- routes/api.php
+++ routes/api.php
@@ -5,21 +5,9 @@ use App\Http\Controllers\GiroController;
 use App\Http\Controllers\OrdenController;
 use App\Http\Controllers\SegmentoController;
 use App\Http\Controllers\SucursalController;
-use App\Http\Controllers\TaskController;
 use App\Http\Controllers\VehiculoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
